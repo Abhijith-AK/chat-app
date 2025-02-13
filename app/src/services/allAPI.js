5cb152cef29dc3f4f24b20a766511b28675591cf
Abhijith Krishna A K <138287385+Abhijith-AK@users.noreply.github.com>
2025-02-14 00:07:20 +0530

    final commit

--- app/src/services/allAPI.js
+++ app/src/services/allAPI.js
@@ -2,21 +2,21 @@ import commonAPI from "./commonAPI"
 import SERVERURL from "./SERVERURL"
 
 export const userLogin = async (reqBody) => {
-    return await commonAPI("POST", `${SERVERURL}/api/login`, reqBody)
+    return await commonAPI("POST", `/api/login`, reqBody)
 }
 
 export const userRegister = async (reqBody) => {
-    return await commonAPI("POST", `${SERVERURL}/api/register`, reqBody)
+    return await commonAPI("POST", `/api/register`, reqBody)
 }
 
 export const getAllUsers = async () => {
-    return await commonAPI("GET", `${SERVERURL}/api/users`, {})
+    return await commonAPI("GET", `/api/users`, {})
 }
 
 export const getAllMessages = async (reqBody) => {
-    return await commonAPI("POST", `${SERVERURL}/api/all`, reqBody)
+    return await commonAPI("POST", `/api/all`, reqBody)
 }
 
 export const sendMessage = async (reqBody) => {
-    return await commonAPI("POST", `${SERVERURL}/api/send`, reqBody)
+    return await commonAPI("POST", `/api/send`, reqBody)
 }
